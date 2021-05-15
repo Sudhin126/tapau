4bac6a42232b8047c06aa14209c8cdf260d91c44
Sudhin Ps <sudhinsamyak@gamil.com>
2021-05-15 13:06:00 +0530

    second

--- src/app/addpackages-dashboard/addpackages-dashboard.component.ts
+++ src/app/addpackages-dashboard/addpackages-dashboard.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addpackages-dashboard.component.css']
 })
 export class AddpackagesDashboardComponent implements OnInit {
+  files: File[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSelect(event) {
+		console.log(event);
+		this.files.push(...event.addedFiles);
+	}
+
+	onRemove(event) {
+		console.log(event);
+		this.files.splice(this.files.indexOf(event), 1);
+	}
+
 
 }
